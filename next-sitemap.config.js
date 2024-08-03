e1383f4df69eb68e8e704c45441a973689a83c96
qudgus21 <qudgus21>
2024-08-03 23:34:24 +0900

    fix: sitemap 수정

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,8 @@
-// next-sitemap.config.js
 module.exports = {
-    siteUrl: 'https://www.thumbnailvideo.com', // 당신의 사이트 URL
+    siteUrl: 'https://www.thumbnailvideo.com', // 웹사이트의 기본 URL
     generateRobotsTxt: true, // robots.txt 파일을 생성합니다
-    sitemapSize: 7000, // (옵션) 사이트 맵 파일당 URL 수 제한 (기본값: 50000)
-    changefreq: 'yearly', // 페이지 변경 빈도를 거의 없다고 설정 (매년)
-    priority: 1.0, // 페이지의 중요도 (1.0은 가장 높음)
-    // 추가 옵션을 설정할 수 있습니다
+    sitemapSize: 7000, // 한 파일에 포함할 최대 URL 수 (필요에 따라 조정)
+    changefreq: 'yearly', // 페이지 변경 빈도
+    priority: 1.0, // 페이지의 중요도
+    generateIndexSitemap: false, // 사이트맵 인덱스 파일 생성을 비활성화
 };
